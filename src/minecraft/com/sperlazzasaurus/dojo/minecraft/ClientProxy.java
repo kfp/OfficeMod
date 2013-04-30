fa7cc769d59467ccc36ef992bd109e15cbc67c27
Elliott Sperlazza <esperlaz@localhost.localdomain>
2013-04-30 08:00:37 -0400

    init import

---
+++ src/minecraft/com/sperlazzasaurus/dojo/minecraft/ClientProxy.java
@@ -0,0 +1,10 @@
+package com.sperlazzasaurus.dojo.minecraft;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy {
+    @Override
+    public void registerRenderers() {
+            MinecraftForgeClient.preloadTexture(ITEMS_PNG);
+    }
+}
---
+++ src/minecraft/com/sperlazzasaurus/dojo/minecraft/CommonProxy.java
@@ -0,0 +1,10 @@
+package com.sperlazzasaurus.dojo.minecraft;
+
+public class CommonProxy {
+    public static String ITEMS_PNG = "/common/resources/items/textures/tpsReport.png";
+   
+    // Client stuff
+    public void registerRenderers() {
+            // Nothing here as the server doesn't render graphics!
+    }
+}
